029d33b8e03f0a8dc3363d6335fb9558f1e028e5
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-04-20 21:32:27 +0000

    fix: foo

--- src/generate.js
+++ src/generate.js
@@ -5,44 +5,20 @@ import { omit } from 'lodash-es';
 
 import revertCompilerChanges from './revert-compiler-changes.js';
 
-const importPlugins = async (plugins = {}) => {
-  const resolvedPlugins = {};
-
-  for (const [name, plugin] of Object.entries(plugins)) {
-    if (plugin) {
-      resolvedPlugins[name] = {
-        instance:
-          plugin.instance ||
-          (await import(
-            /* @vite-ignore */
-            name
-          ).then(m => m.default || m)),
-        options: plugin.options || {},
-      };
-    } else {
-      resolvedPlugins[name] = false;
-    }
-  }
-
-  return resolvedPlugins;
-};
-
 export default async (file, options = {}, markdownOptions = {}) => {
   options = defu(options, markdownOptions);
 
+  /**
+   * When we disable highlight via the module, it was already enabled before and the highlight plugin was added.
+   * So we need to remove it again.
+   */
   if (options.highlight === false) {
     options.rehypePlugins = omit(options.rehypePlugins, ['highlight']);
   }
 
-  options.rehypePlugins = await importPlugins(options.rehypePlugins);
-  options.remarkPlugins = await importPlugins(options.remarkPlugins);
-
   const parsed = await parseMarkdown(file.rawbody || file.body, {
     ...options,
-    rehype: {
-      plugins: options.rehypePlugins,
-      // options: { handlers: { link } }
-    },
+    rehype: { plugins: options.rehypePlugins },
     remark: { plugins: options.remarkPlugins },
   });
 
